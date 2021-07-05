d0dceff759137122dd5e9afcad3e8ca36f156085
steepcurve <steepcurve@163.com>
2021-07-05 20:43:35 +0800

    update

--- src/dp/_312_burst_balloons.rs
+++ src/dp/_312_burst_balloons.rs
@@ -1,4 +1,5 @@
 use std::cmp::max;
+use std::collections::VecDeque;
 
 struct Solution;
 
@@ -44,9 +45,38 @@ impl Solution {
         dp[left][right] = Some(res);
         res
     }
+
+    fn dp(nums: Vec<i32>) -> i32 {
+        if nums.is_empty() {
+            return 0;
+        }
+        if nums.len() == 1 {
+            return nums[0];
+        }
+        let n = nums.len();
+        let mut nums = nums.into_iter().collect::<VecDeque<_>>();
+        nums.push_back(1);
+        nums.push_front(1);
+        let mut dp = vec![vec![0; n + 1]; n + 1];
+        for i in 1..=n {
+            dp[i][i] = nums[i - 1] * nums[i] * nums[i + 1];
+        }
+        for i in (1..n).rev() {
+            for j in i + 1..=n {
+                dp[i][j] = (dp[i + 1][j] + nums[i - 1] * nums[i] * nums[j + 1])
+                    .max(dp[i][j - 1] + nums[i - 1] * nums[j] * nums[j + 1]);
+                for k in i + 1..j {
+                    dp[i][j] = dp[i][j]
+                        .max(dp[i][k - 1] + dp[k + 1][j] + nums[i - 1] * nums[k] * nums[j + 1]);
+                }
+            }
+        }
+        dp[1][n]
+    }
 }
 
 #[test]
 fn test() {
     assert_eq!(Solution::max_coins(vec![3, 1, 5, 8]), 167);
+    assert_eq!(Solution::dp(vec![3, 1, 5, 8]), 167);
 }
